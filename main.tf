4ba7095fd458e36256ecdb74c7a4a13867c05e3b
deltadan <dan@solliance.net>
2022-11-17 18:23:30 -0500

    update to include encryption by default

--- main.tf
+++ main.tf
@@ -16,19 +16,18 @@ module "name" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  account_replication_type        = "ZRS"
-  account_tier                    = "Standard"
-  allow_nested_items_to_be_public = false
-  enable_https_traffic_only       = true
-  location                        = var.resource_group.location
-  min_tls_version                 = "TLS1_2"
-  name                            = "satst${module.name.resource_suffix_compact}"
-  resource_group_name             = var.resource_group.name
-  tags                            = local.tags
+  account_replication_type          = "ZRS"
+  account_tier                      = "Standard"
+  allow_nested_items_to_be_public   = false
+  enable_https_traffic_only         = true
+  infrastructure_encryption_enabled = true
+  location                          = var.resource_group.location
+  min_tls_version                   = "TLS1_2"
+  name                              = "satst${module.name.resource_suffix_compact}"
+  resource_group_name               = var.resource_group.name
+  tags                              = local.tags
 
   blob_properties {
-    change_feed_enabled           = true
-    change_feed_retention_in_days = 30
     container_delete_retention_policy { days = 30 }
     delete_retention_policy { days = 30 }
     versioning_enabled = true
